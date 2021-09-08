9f42c333184aa9049b584f4e897949dc64906ba2
maducking <duckingura@gmail.com>
2021-09-08 20:12:05 +0900

    헤로쿠 일치화

--- app/bin/www.js
+++ app/bin/www.js
@@ -2,8 +2,12 @@
 
 //중요!! 서버켰을 때 같은 폴더에 json 파일을 읽어야 하므로 json 파일이 있는 폴더에서 실행을 해야함
 //그렇지 않을때 view폴더를 볼 수가 없다고 나옴
+// const app = require("../main");
+// const PORT = 3000;
+
+//heroku setting
 const app = require("../main");
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, ()=>{
     console.log('UCDL 서버가동!');
--- app/main.js
+++ app/main.js
@@ -1,8 +1,10 @@
 "use strict";
 
 // 모듈
-const express = require('express');
 const app = express();
+const express = require('express');
+const home = require("./src/routes/home");      //home router 세팅
+
 // const bodyParser =require('body-parser');
 
 //engine
@@ -14,9 +16,7 @@ app.set('view engine', 'ejs');            // ejs engine 세팅
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const home = require("./src/routes/home");      //home router 세팅
 app.use("/", home);                             // "/"루트경로로 들어오면 변수 "home"의 경로로 자동으로 들어간다.
-
 app.use(express.static(__dirname + '/src/public'));
 // app.use(express.static(__dirname + '/src/js/home'));
 
